6ac2306312ddba3eb9c9dd54f7627fb8d3d0eaf3
LDom890 <montecinonestor890@gmail.com>
2023-02-22 16:54:30 -0600

    Add other Joy

--- PixelDestroyMoblie/Assets/_Master/Scripts/InputController.cs
+++ PixelDestroyMoblie/Assets/_Master/Scripts/InputController.cs
@@ -3,24 +3,44 @@ using UnityEngine;
 
 public class InputController : MonoBehaviour
 {
-    
-    public float velocidadGiro = 0.2f;
-    public VirtualJoystick joystickGiro;
-    
-    public Transform player;
+    public Joystick joystick;
+    public Joystick joystickR;
 
+    public int velocidad;
+    public int velocidadR;
+
+    public Rigidbody rb;
+    public Rigidbody player;
+
+    public bool ConFisicas;
+    
     private void Start()
     {
         
     }
-    /*
+    private void FixedUpdate()
+    {
+      
+      Rotate();
+        Vector2 direction = Vector2.up * joystick.Vertical + Vector2.right * joystick.Horizontal;
+        if(ConFisicas)
+        {
+            rb.AddForce(direction * velocidad * Time.fixedDeltaTime, ForceMode.Impulse);
+        }
+        else
+        {
+            gameObject.transform.Translate(direction * velocidad * Time.deltaTime);
+        }
+    }
+    
     void Rotate()
     {
-        float rotateV;
-        float rotateH;
-        rotateH = joystickGiro.Horizontal * velocidadGiro;
-        rotateV = joystickGiro.Vertical * velocidadGiro;
-        player.Rotate(0, rotateH, 0);
+        Vector2 rotation = new Vector2(joystickR.Vertical * 50 * Time.deltaTime, joystickR.Horizontal * 50*Time.deltaTime);
+        rotation.y= Mathf.Clamp(rotation.y,-45, 45);
+        rotation.x = Mathf.Clamp(rotation.x, -45,45 );
+        player.transform.Rotate(rotation);
+       
+
     }
-    */
+
 }
